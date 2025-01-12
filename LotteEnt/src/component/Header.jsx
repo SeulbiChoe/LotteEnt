74e5735a2e694a4c6a4bce0c26c077953a95cc2b
Jiji <102505581+SeulbiChoe@users.noreply.github.com>
2025-01-12 22:43:23 +0900

    [seulbi]_[header]

--- LotteEnt/src/component/Header.jsx
+++ LotteEnt/src/component/Header.jsx
@@ -1,3 +1,27 @@
-const Header = () => {};
+import { useMediaQuery } from "react-responsive";
+
+const Desktop = ({ children }) => {
+  const isDesktop = useMediaQuery({ minWidth: 1024 });
+  return isDesktop ? children : null;
+};
+const Tablet = ({ children }) => {
+  const isTablet = useMediaQuery({ minWidth: 768, maxWidth: 1023 });
+  return isTablet ? children : null;
+};
+const Mobile = ({ children }) => {
+  const isMobile = useMediaQuery({ maxWidth: 767 });
+  return isMobile ? children : null;
+};
+const Header = () => {
+  return (
+    <header>
+      <Desktop>Desktop or laptop</Desktop>
+      <Tablet>Tablet</Tablet>
+      <Mobile>
+        <div>children mobile</div>
+      </Mobile>
+    </header>
+  );
+};
 
 export default Header;
